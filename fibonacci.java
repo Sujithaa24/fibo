dd023c76aec5889279b79cfef0a662965dbb2499
sujithaa24 <sujithaa075@gmail.com>
2025-03-14 10:33:16 +0530

    first commit

---
+++ fibonacci.java
@@ -0,0 +1,14 @@
+ 
+class fibonacci{
+    public static void main (String args[]){
+    
+        int num1=0;
+        int num2=1;
+        int num3 = num2+num1;
+        System.out.println("the fibonacci series of first 3 number");
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    
+}
+}
