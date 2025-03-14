e6a64c0e68e1d5d75eef59666d961321d2414f79
Sujithaa08 <jsujithaa96@gmail.com>
2025-03-14 11:00:31 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -5,7 +5,7 @@ class fibonacci{
         int arr[] num1=0;
         int arr[] num2=1;
         int arr[] num3 = num2+num1;
-        System.out.println("the fibonacci series of first 3 number");
+        System.out.println("the fibonacci series of first 3 numberss");
         System.out.println(arr[]num1);
         System.out.println(arr[]num2);
         System.out.println(arr[]num3);
